ffaab092b4d039e0fe1443bb94e2af490b7f6863
yonono <30824451+ononoy@users.noreply.github.com>
2017-09-25 17:49:01 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,49 @@
+/**
+ *
+ */
+
+/**
+ * @author y
+ *
+ */
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!!");
+
+		///////////////////
+
+	    Scanner scanner = new Scanner(System.in);
+
+	    System.out.print("名前：");
+	    String firstName = scanner.next();
+
+	    System.out.print("名字：");
+	    String lastName = scanner.next();
+
+	    System.out.print("年齢：");
+	    int age = scanner.nextInt();
+
+	    System.out.print("身長(m)：");
+	    double height = scanner.nextDouble();
+
+	    System.out.print("体重(kg)：");
+	    double weight = scanner.nextDouble();
+
+	    Person.printData(firstName, lastName, age, height, weight);
+
+
+
+
+
+	}
+
+}
+
 
